5a5354e278f1cd659765a45d1c52f97a4580e1c1
Jason Elder <jase.elder@gmail.com>
2024-03-07 21:19:24 +0000

    - fixed bug where the last section on an accordion that is larger than the viewable window won't scroll its contents up to be viewable.

--- lib/accordion_section.dart
+++ lib/accordion_section.dart
@@ -88,11 +88,9 @@ class AccordionSection extends StatelessWidget with CommonParams {
     sectionCtrl.isSectionOpen.value = listCtrl.openSections.contains(uniqueKey);
 
     this.headerBackgroundColor = headerBackgroundColor;
-    this.headerBackgroundColorOpened =
-        headerBackgroundColorOpened ?? headerBackgroundColor;
+    this.headerBackgroundColorOpened = headerBackgroundColorOpened ?? headerBackgroundColor;
     this.headerBorderColor = headerBorderColor ?? headerBackgroundColor;
-    this.headerBorderColorOpened =
-        headerBorderColorOpened ?? headerBackgroundColorOpened;
+    this.headerBorderColorOpened = headerBorderColorOpened ?? headerBackgroundColorOpened;
     this.headerBorderWidth = headerBorderWidth;
     this.headerBorderRadius = headerBorderRadius;
     this.headerPadding = headerPadding;
@@ -108,8 +106,7 @@ class AccordionSection extends StatelessWidget with CommonParams {
     this.contentVerticalPadding = contentVerticalPadding;
     this.paddingBetweenOpenSections = paddingBetweenOpenSections;
     this.paddingBetweenClosedSections = paddingBetweenClosedSections;
-    this.scrollIntoViewOfItems =
-        scrollIntoViewOfItems ?? ScrollIntoViewOfItems.fast;
+    this.scrollIntoViewOfItems = scrollIntoViewOfItems ?? ScrollIntoViewOfItems.fast;
     this.sectionOpeningHapticFeedback = sectionOpeningHapticFeedback;
     this.sectionClosingHapticFeedback = sectionClosingHapticFeedback;
     this.accordionId = accordionId;
@@ -122,14 +119,12 @@ class AccordionSection extends StatelessWidget with CommonParams {
   /// getter to flip the widget vertically (Icon by default)
   /// on the left of this section header to visually indicate
   /// if this section is open or closed
-  get _flipQuarterTurnsLeft =>
-      SectionController.flipLeftIconIfOpen && _isOpen ? 2 : 0;
+  get _flipQuarterTurnsLeft => SectionController.flipLeftIconIfOpen && _isOpen ? 2 : 0;
 
   /// getter to flip the widget vertically (Icon by default)
   /// on the right of this section header to visually indicate
   /// if this section is open or closed
-  get _flipQuarterTurnsRight =>
-      SectionController.flipRightIconIfOpen && _isOpen ? 2 : 0;
+  get _flipQuarterTurnsRight => SectionController.flipRightIconIfOpen && _isOpen ? 2 : 0;
 
   /// getter indication the open or closed status of this section
   get _isOpen {
@@ -137,14 +132,10 @@ class AccordionSection extends StatelessWidget with CommonParams {
     final open = sectionCtrl.isSectionOpen.value;
 
     Timer(
-      sectionCtrl.firstRun
-          ? (listCtrl.initialOpeningSequenceDelay + min(index * 200, 1000))
-              .milliseconds
-          : 0.seconds,
+      sectionCtrl.firstRun ? (listCtrl.initialOpeningSequenceDelay + min(index * 200, 1000)).milliseconds : 0.seconds,
       () {
         if (Accordion.sectionAnimation) {
-          sectionCtrl.controller
-              .fling(velocity: open ? 1 : -1, springDescription: springFast);
+          sectionCtrl.controller.fling(velocity: open ? 1 : -1, springDescription: springFast);
         } else {
           sectionCtrl.controller.value = open ? 1 : 0;
         }
@@ -157,8 +148,7 @@ class AccordionSection extends StatelessWidget with CommonParams {
 
   /// play haptic feedback when opening/closing sections
   _playHapticFeedback(bool opening) {
-    final feedback =
-        opening ? sectionOpeningHapticFeedback : sectionClosingHapticFeedback;
+    final feedback = opening ? sectionOpeningHapticFeedback : sectionClosingHapticFeedback;
 
     switch (feedback) {
       case SectionHapticFeedback.light:
@@ -208,20 +198,14 @@ class AccordionSection extends StatelessWidget with CommonParams {
                 listCtrl.updateSections(uniqueKey);
                 _playHapticFeedback(_isOpen);
 
-                if (_isOpen &&
-                    scrollIntoViewOfItems != ScrollIntoViewOfItems.none &&
-                    listCtrl.controller.hasClients) {
+                if (!_isOpen && scrollIntoViewOfItems != ScrollIntoViewOfItems.none && listCtrl.controller.hasClients) {
                   Timer(
                     250.milliseconds,
                     () {
                       listCtrl.controller.cancelAllHighlights();
                       listCtrl.controller.scrollToIndex(index,
                           preferPosition: AutoScrollPosition.middle,
-                          duration: (scrollIntoViewOfItems ==
-                                      ScrollIntoViewOfItems.fast
-                                  ? .5
-                                  : 1)
-                              .seconds);
+                          duration: (scrollIntoViewOfItems == ScrollIntoViewOfItems.fast ? .5 : 1).seconds);
                     },
                   );
                 }
@@ -233,30 +217,21 @@ class AccordionSection extends StatelessWidget with CommonParams {
                 }
               },
               child: AnimatedContainer(
-                duration: Accordion.sectionAnimation
-                    ? 750.milliseconds
-                    : 0.milliseconds,
+                duration: Accordion.sectionAnimation ? 750.milliseconds : 0.milliseconds,
                 curve: Curves.easeOut,
                 alignment: Alignment.center,
                 padding: headerPadding,
                 decoration: BoxDecoration(
-                  color: (_isOpen
-                          ? headerBackgroundColorOpened
-                          : headerBackgroundColor) ??
-                      Theme.of(context).primaryColor,
+                  color:
+                      (_isOpen ? headerBackgroundColorOpened : headerBackgroundColor) ?? Theme.of(context).primaryColor,
                   borderRadius: BorderRadius.vertical(
                     top: Radius.circular(borderRadius),
                     bottom: Radius.circular(_isOpen ? 0 : borderRadius),
                   ),
                   border: Border.all(
-                    color: (_isOpen
-                            ? headerBorderColorOpened
-                            : headerBorderColor) ??
-                        Theme.of(context).primaryColor,
+                    color: (_isOpen ? headerBorderColorOpened : headerBorderColor) ?? Theme.of(context).primaryColor,
                     width: (headerBorderWidth ?? 0),
-                    style: (headerBorderWidth ?? 0) <= 0
-                        ? BorderStyle.none
-                        : BorderStyle.solid,
+                    style: (headerBorderWidth ?? 0) <= 0 ? BorderStyle.none : BorderStyle.solid,
                   ),
                 ),
                 child: Row(
@@ -269,8 +244,7 @@ class AccordionSection extends StatelessWidget with CommonParams {
                     Expanded(
                       flex: 10,
                       child: Padding(
-                        padding: EdgeInsets.symmetric(
-                            horizontal: leftIcon == null ? 0 : 15),
+                        padding: EdgeInsets.symmetric(horizontal: leftIcon == null ? 0 : 15),
                         child: _isOpen ? headerOpen ?? header : header,
                       ),
                     ),
@@ -290,23 +264,17 @@ class AccordionSection extends StatelessWidget with CommonParams {
           ),
           Padding(
             padding: EdgeInsets.only(
-                bottom: _isOpen
-                    ? paddingBetweenOpenSections ?? 10
-                    : paddingBetweenClosedSections ?? 10),
+                bottom: _isOpen ? paddingBetweenOpenSections ?? 10 : paddingBetweenClosedSections ?? 10),
             child: SizeTransition(
               sizeFactor: sectionCtrl.controller,
               child: ScaleTransition(
-                scale: Accordion.sectionScaleAnimation
-                    ? sectionCtrl.controller
-                    : const AlwaysStoppedAnimation(1.0),
+                scale: Accordion.sectionScaleAnimation ? sectionCtrl.controller : const AlwaysStoppedAnimation(1.0),
                 child: Center(
                   child: Container(
                     clipBehavior: Clip.antiAlias,
                     decoration: BoxDecoration(
-                      color:
-                          contentBorderColor ?? Theme.of(context).primaryColor,
-                      borderRadius: BorderRadius.vertical(
-                          bottom: Radius.circular(contentBorderRadius)),
+                      color: contentBorderColor ?? Theme.of(context).primaryColor,
+                      borderRadius: BorderRadius.vertical(bottom: Radius.circular(contentBorderRadius)),
                     ),
                     child: Padding(
                       padding: EdgeInsets.fromLTRB(
@@ -319,16 +287,12 @@ class AccordionSection extends StatelessWidget with CommonParams {
                         clipBehavior: Clip.antiAlias,
                         decoration: BoxDecoration(
                             color: Colors.white,
-                            borderRadius: BorderRadius.vertical(
-                                bottom: Radius.circular(
-                                    contentBorderRadius / 1.02))),
+                            borderRadius: BorderRadius.vertical(bottom: Radius.circular(contentBorderRadius / 1.02))),
                         child: Container(
                           clipBehavior: Clip.antiAlias,
                           decoration: BoxDecoration(
                               color: contentBackgroundColor,
-                              borderRadius: BorderRadius.vertical(
-                                  bottom: Radius.circular(
-                                      contentBorderRadius / 1.02))),
+                              borderRadius: BorderRadius.vertical(bottom: Radius.circular(contentBorderRadius / 1.02))),
                           child: Padding(
                             padding: EdgeInsets.symmetric(
                               horizontal: contentHorizontalPadding ?? 10,
